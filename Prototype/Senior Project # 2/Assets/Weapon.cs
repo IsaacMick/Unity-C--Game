a04bd773935a7584b68389e17383f126e1f29c72
IsaacMick <43608583+IsaacMick@users.noreply.github.com>
2019-03-07 03:07:19 -0500

    Shooting
    Character can now shoot a pistol

---
+++ Prototype/Senior Project # 2/Assets/Weapon.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    public float fireRate = 0;
+    public float damage = 10;
+    public LayerMask hittable;
+
+    float timeToFire = 0;
+    Transform firePoint;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        firePoint = transform.Find("FirePoint");
+        if (firePoint == null)
+        {
+            Debug.LogError("NO FIRE POINT DETECTED");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (fireRate == 0)
+        {
+            if (Input.GetButtonDown("Fire1"))
+            {
+                Shoot();
+            }
+        }
+
+        else
+        {
+            if(Input.GetButtonDown("Fire1") && Time.time > timeToFire)
+            {
+                timeToFire = Time.time + 1 / fireRate;
+                Shoot();
+            }
+        }
+    }
+
+    void Shoot()
+    {
+        Vector2 mousePosition = new Vector2(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y);
+        Vector2 firePointPos = new Vector2(firePoint.position.x, firePoint.position.y);
+        RaycastHit2D hit = Physics2D.Raycast(firePointPos, mousePosition - firePointPos, 100, hittable);
+
+        Debug.DrawLine(firePointPos, (mousePosition - firePointPos) * 100, Color.cyan);
+        if (hit.collider != null)
+        {
+            Debug.DrawLine(firePointPos, hit.point, Color.red);
+            Debug.Log("HIT: " + hit.collider.name);
+        }
+    }
+}
